8ee0415615243f1452eecca2d8ba6275365bb2a8
knowcup <knowcup@163.com>
2018-04-09 08:56:27 +0800

    初始化

---
+++ src/Tree.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Description:
+ * Motto: 知杯者，无惧.
+ * Author: Captain <QQ:497439092>
+ * Date: 2018/4/8
+ * Time: 16:58
+ */
+
+namespace knowcup\tree;
+
+
+class Tree
+{
+    public function test(){
+        echo "knowcup tree is ok";
+    }
+}
